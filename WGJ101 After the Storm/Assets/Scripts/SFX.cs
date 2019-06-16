ea8bf3a674fbed3b423a8e29898ed2090a8a02de
Baradoros <thatfunderburkguy@gmail.com>
2019-06-16 16:49:38 -0500

    Basic SFX funtionality

---
+++ WGJ101 After the Storm/Assets/Scripts/SFX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SFX : MonoBehaviour {
+    public AudioClip[] audioClips;
+    //public LayerMask collisionLayers;
+
+    private AudioSource audioSource;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private AudioClip ChooseAudioClip() {
+        return audioClips[Random.Range(0, audioClips.Length)];
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        audioSource.clip = ChooseAudioClip();
+        audioSource.Play();
+    }
+}
