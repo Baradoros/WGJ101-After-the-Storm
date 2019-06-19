9bf3b0eca91e865bfc99e32e477eb6d9c49786d2
Baradoros <thatfunderburkguy@gmail.com>
2019-06-19 14:08:33 -0500

    Reenabled audio

--- WGJ101 After the Storm/Assets/Scripts/SFX.cs
+++ WGJ101 After the Storm/Assets/Scripts/SFX.cs
@@ -8,17 +8,25 @@ public class SFX : MonoBehaviour {
     //public LayerMask collisionLayers;
 
     private AudioSource audioSource;
+    private Rigidbody rb;
 
     private void Start() {
+        rb = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
-    private AudioClip ChooseAudioClip() {
+    private AudioClip ChooseRandomAudioClip() {
         return audioClips[Random.Range(0, audioClips.Length)];
     }
 
     private void OnCollisionEnter(Collision collision) {
-        audioSource.clip = ChooseAudioClip();
+        // No SFX if barely moving
+        //if (rb?.velocity.magnitude <= 0.5f) {
+        //    return;
+        //}
+
+        audioSource.clip = ChooseRandomAudioClip();
+
         audioSource.Play();
     }
 }
